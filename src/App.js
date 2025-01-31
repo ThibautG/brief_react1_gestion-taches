8f8dcf06d9fb4009d3b4e53831e704e5435780db
ThibautG <thibaut.gaudinat@gmail.com>
2025-01-31 16:29:21 +0100

    récupérer la value de input et ajouter dans un tableau au click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    const [notes, SetNotes] = useState([]);
+    const [newNotes, SetNewNotes] = useState('');
 
-      </header>
+    function handleClick() {
+        SetNotes([...notes, newNotes]);
+    }
+
+    console.log(newNotes);
+    console.log(notes);
+
+    return (
+    <div className="App">
+        <header className="App-header">
+            <h1>Gestion de tâches</h1>
+            <label htmlFor="task">Rentrez une tâche</label>
+            <input onChange={e => SetNewNotes(e.target.value)}/>
+            <button onClick={handleClick}>Ajouter</button>
+            <p>Vous avez ajouté la tache : {newNotes}</p>
+            <div id={"taskList"}></div>
+        </header>
     </div>
+
+
   );
 }
 
