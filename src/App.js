5326a72dbc1a8cb7d89f8700512f638d0fd7b9c9
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-03 10:31:59 +0100

    ajout de la fonction delete et des commentaires

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-    const [notes, SetNotes] = useState([]);
+    // on stocke ce que tape l'utilisateur dans champ (string)
     const [newNotes, SetNewNotes] = useState('');
+    // on stocke la liste de tâches (tableau de strings)
+    const [notes, SetNotes] = useState([]);
 
     function handleClick() {
+        // on va utiliser cette fonction sur le bouton ajouter la tâche
+        // on modifie un copie du tableau notes en lui ajoutant newNotes
+        // newNotes correspond à ce qui est entré par l'utilisateur dans le champ
         SetNotes([...notes, newNotes]);
     }
 
-    console.log(newNotes);
-    console.log(notes);
+    function deleteId(index) {
+        // on va utiliser cette fonction sur le bouton supprimer
+        // on crée une copie dynamique du tableau notes
+        const liste = [...notes];
+        // on supprime la ligne de ce tableau ayant pour index ce qu'on a rentré en argument
+        liste.splice(index, 1);
+        // on réaffecte ce nouveau tableau à la variable notes via SetNotes
+        SetNotes(liste);
+    }
 
     return (
     <div className="App">
@@ -19,8 +31,13 @@ function App() {
             <label htmlFor="task">Rentrez une tâche</label>
             <input onChange={e => SetNewNotes(e.target.value)}/>
             <button onClick={handleClick}>Ajouter</button>
-            <p>Vous avez ajouté la tache : {newNotes}</p>
-            <div id={"taskList"}></div>
+            <ul>
+                {notes.map((produit, id) => (
+                <li key={id}>
+                    {produit} <button onClick={() => deleteId(id)}>Supprimer</button>
+                </li>
+                ))}
+            </ul>
         </header>
     </div>
 
